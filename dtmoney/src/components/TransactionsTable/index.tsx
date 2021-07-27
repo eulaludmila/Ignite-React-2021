cdcaf76a16682793df8c192c4b147ff65e53aa4c
Eula Ludmila <eula.silva.teixeira@gmail.com>
2021-07-27 18:15:08 -0300

    feat: style modal and transactions data

--- dtmoney/src/components/TransactionsTable/index.tsx
+++ dtmoney/src/components/TransactionsTable/index.tsx
@@ -1,15 +1,25 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { Container } from "./styles"
 import { api } from "../../services/api"
 
-export function TransactionsTable(){
+interface Transaction {
+  id: number,
+  amount: number,
+  title: string,
+  category: string,
+  type: string,
+  createdAt: string,
+}
+
+export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
 
   useEffect(() => {
     api.get('/transactions')
-    .then(response => console.log(response.data))
-  },[])
+      .then(response => setTransactions(response.data.transactions))
+  }, [])
 
-  return(
+  return (
     <Container>
       <table>
         <thead>
@@ -21,18 +31,23 @@ export function TransactionsTable(){
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Desenvolvimento de Website</td>
-            <td className="deposit">R$12.000,00</td>
-            <td>Desenvolvimento</td>
-            <td>10/01/2021</td>
-          </tr>
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw">- R$1.100,00</td>
-            <td>Casa</td>
-            <td>17/02/2021</td>
-          </tr>
+          {
+            transactions.map(transaction =>
+              <tr key={transaction.id}>
+                <td>{transaction.title}</td>
+                <td className={transaction.type}>
+                  {new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  }).format(transaction.amount)}
+                </td>
+                <td>{transaction.category}</td>
+                <td>
+                  {new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}
+                </td>
+              </tr>
+            )
+          }
         </tbody>
       </table>
     </Container>
