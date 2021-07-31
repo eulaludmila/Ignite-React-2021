5981132a8299f5025277c78a517f5565b3eb5cf6
Eula Ludmila <eula.silva.teixeira@gmail.com>
2021-07-30 21:22:41 -0300

    feat: insert, updateList and hooks

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,10 @@
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
-import { FormEvent, useState } from 'react';
-import { api } from '../../services/api';
+import { useTransactions } from '../../hooks/useTransactions';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -12,18 +12,24 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const {createTransaction} = useTransactions()
   const [title, setTitle] = useState('');
   const [category, setCategory] = useState('');
-  const [value, setValue] = useState(0);
+  const [amount, setAmount] = useState(0);
   const [type, setType] = useState('deposit');
 
-  function handleCreateNewTransaction(e:FormEvent) {
+  async function handleCreateNewTransaction(e:FormEvent) {
     e.preventDefault();
 
-    const data = {title, category, value, type};
-
-    api.post('/transactions', data);
+    await createTransaction(
+      {title, category, amount, type}
+      );
     
+    setTitle('');
+    setAmount(0);
+    setCategory('');
+    setType('deposit');
+    onRequestClose()
   }
 
   return (
@@ -39,7 +45,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
       <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar Transação</h2>
         <input placeholder="Título" value={title} onChange={e => setTitle(e.target.value)}/>
-        <input placeholder="Valor" type="number"  value={value} onChange={e => setValue(Number(e.target.value))}/>
+        <input placeholder="Valor" type="number"  value={amount} onChange={e => setAmount(Number(e.target.value))}/>
 
         <TransactionTypeContainer>
           <RadioBox
---
+++ dtmoney/src/hooks/useTransactions.tsx
@@ -0,0 +1,68 @@
+import { createContext, useEffect, useState, ReactNode, useContext } from "react";
+import { api } from "../services/api";
+
+interface Transaction {
+  id: number,
+  amount: number,
+  title: string,
+  category: string,
+  type: string,
+  createdAt: string,
+}
+
+interface TransactionsProps{
+  children: ReactNode;
+}
+
+// interface TransactionInput{
+//   title:string,
+//   category:string,
+//   value:number,
+//   type:string,
+// }
+
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>
+
+interface TransactionsContextData {
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
+}
+
+const TransactionsContext = createContext<TransactionsContextData>(
+  {} as TransactionsContextData
+  );
+
+
+export function TransactionsProvider({children}: TransactionsProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  useEffect(() => {
+    api.get('/transactions')
+      .then(response => setTransactions(response.data.transactions))
+  }, [])
+
+  async function createTransaction(transactionInput: TransactionInput){
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date()
+    });
+    const { transaction } = response.data;
+
+    setTransactions([
+      ...transactions,
+      transaction,
+    ])
+  }
+
+  return (
+    <TransactionsContext.Provider value={{transactions, createTransaction}}>
+      {children}
+    </TransactionsContext.Provider>
+  )
+}
+
+export function useTransactions(){
+  const context = useContext(TransactionsContext)
+
+  return context;
+}
