82e6790c17e80a53004a8c21302f677daecc940e
Eula Ludmila <eula.silva.teixeira@gmail.com>
2022-06-26 21:38:57 -0300

    feat(axios): changed fetch to axios

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -6,12 +6,12 @@ import { Header } from "../../components/Header";
 import Pagination from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 import { useQuery } from 'react-query'
+import { api } from "../../services/api";
 
 export default function UserList() {
   // users - nome da chave que vai ser armazenada no cache do front-end
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users');
-    const data = await response.json();
+  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
+    const { data } = await api.get('users');
 
     const users = data.users.map(user => {
       return {
@@ -57,7 +57,10 @@ export default function UserList() {
           p="8"
         >
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal" >Usuários</Heading>
+            <Heading size="lg" fontWeight="normal" >
+              Usuários
+              { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4"/>}
+            </Heading>
             <Link href="/users/create" passHref>
               <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20" />}>
                 Criar Novo
