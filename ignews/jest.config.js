69375a03bcdedda674e89280ad8196744be7dbf5
Eula Ludmila <eula.silva.teixeira@gmail.com>
2023-10-25 19:48:36 -0300

    feat(tests): configs, test link and header

--- ignews/jest.config.js
+++ ignews/jest.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  testIgnorePatterns: ["/node-modules", '/.next/'],//ignorar pastas que não possuem testes
+  testPathIgnorePatterns: ["/node-modules", '/.next/'],//ignorar pastas que não possuem testes
   transform: { //transforma os arquivos testes de ts para js, fazendo o babel entender
-    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node-modules/babel-jest" //<rootDir> - simboliza a pasta root onde fica o babel
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest" //<rootDir> - simboliza a pasta root onde fica o babel
   },
   setupFilesAfterEnv: [
     "<rootDir>/src/tests/setupTests.ts" //Para o jests executar antes dos tests
   ],
-  testEnvironment: 'jsdom' //indica em qual ambiente os testes estão executando
+  testEnvironment: 'jsdom', //indica em qual ambiente os testes estão executando
   //jsdom - ele cria uma representação dda dom em js para conseguir entender o que foi e o que não foi renderizado em tela
+  moduleNameMapper: {
+    "^.+\\.(scss|css|sass)$": "identify-obj-proxy"
+  }
 }
